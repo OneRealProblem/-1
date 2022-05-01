44f49aab14d7fa9f6e895a7fccb80645cfed6d66
OneRealProblem <104728216+OneRealProblem@users.noreply.github.com>
2022-05-01 18:59:24 +0300

    ProgForIntegral

---
+++ thfckitis.py
@@ -0,0 +1,53 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f(x):
+    return np.sin(2*x)
+
+
+print("Число точек разбиения:")
+tr = int(input())
+print("Способ выбора оснащения:")
+print("1 - левые")
+print("2 - средние")
+print("3 - правые")
+svo = int(input())
+
+a = 0
+b = np.pi
+x = np.arange(a, b, 0.01)
+
+xk = []
+seredina = []
+yk = []
+dlina = (b-a)/tr #длина одного отрезка
+summ = 0
+
+if svo == 1:
+    othod = -0.5*dlina #возвращаемся к левому краю
+    m = "левые"
+elif svo == 3:
+    othod = 0.5*dlina #идём вперёд к правому краю
+    m = "правые"
+else:
+    othod = 0
+    m = "средние"
+
+for i in range(0,tr):
+    xk.append(a+i*dlina)
+    seredina.append(a + (i + 0.5)*dlina) #середины отрезков
+xk.append(b) #точек разбиения больше, чем отрезков
+
+for i in seredina:
+    yk.append(f(i + othod))
+    summ +=f(i+othod)*dlina
+
+plt.plot(x, f(x),'r')
+plt.bar(seredina, yk, width = dlina,edgecolor="k")
+plt.xlabel(r'$x$')
+plt.ylabel(r'$f(x)$')
+plt.title("Точек разбиения: " + str(tr) + ". Оснащение: " + m + ". I = " + str((summ)),fontsize=10 )
+plt.grid(True)
+plt.savefig("tr"+str(tr)+"svo"+str(svo)+".png")
+plt.show()
